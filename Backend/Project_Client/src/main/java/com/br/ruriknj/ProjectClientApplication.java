8136760388a8a488644a99ead31ea47246f5511d
ruriknj <ruriknj@gmail.com>
2020-10-03 15:33:25 -0300

    Entity Cliente e Resource

---
+++ Backend/Project_Client/src/main/java/com/br/ruriknj/ProjectClientApplication.java
@@ -0,0 +1,13 @@
+package com.br.ruriknj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectClientApplication.class, args);
+	}
+
+}
