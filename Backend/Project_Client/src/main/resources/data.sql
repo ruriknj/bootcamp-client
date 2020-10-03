d91fefba2dda4b4cee628523b9f0da9ffbb87f05
ruriknj <ruriknj@gmail.com>
2020-10-03 17:40:45 -0300

    Testando o H2 e populando o banco

--- Backend/Project_Client/src/main/resources/data.sql
+++ Backend/Project_Client/src/main/resources/data.sql
@@ -1,1 +1,13 @@
 INSERT INTO TB_CLIENT (birth_Date, children, cpf, income, name) VALUES (TIMESTAMP WITH TIME ZONE '1994-07-20T10:30:00Z', 2, '12345678901', 6500.0, 'Maria Silva');
+INSERT INTO TB_CLIENT (birth_Date, children, cpf, income, name) VALUES (TIMESTAMP WITH TIME ZONE '1994-07-20T10:30:00Z', 2, '12345678901', 6500.0, 'Maria Silva');
+INSERT INTO TB_CLIENT (birth_Date, children, cpf, income, name) VALUES (TIMESTAMP WITH TIME ZONE '1994-07-20T10:30:00Z', 2, '12345678901', 6500.0, 'Maria Silva');
+INSERT INTO TB_CLIENT (birth_Date, children, cpf, income, name) VALUES (TIMESTAMP WITH TIME ZONE '1994-07-20T10:30:00Z', 2, '12345678901', 6500.0, 'Maria Silva');
+INSERT INTO TB_CLIENT (birth_Date, children, cpf, income, name) VALUES (TIMESTAMP WITH TIME ZONE '1994-07-20T10:30:00Z', 2, '12345678901', 6500.0, 'Maria Silva');
+INSERT INTO TB_CLIENT (birth_Date, children, cpf, income, name) VALUES (TIMESTAMP WITH TIME ZONE '1994-07-20T10:30:00Z', 2, '12345678901', 6500.0, 'Maria Silva');
+INSERT INTO TB_CLIENT (birth_Date, children, cpf, income, name) VALUES (TIMESTAMP WITH TIME ZONE '1994-07-20T10:30:00Z', 2, '12345678901', 6500.0, 'Maria Silva');
+INSERT INTO TB_CLIENT (birth_Date, children, cpf, income, name) VALUES (TIMESTAMP WITH TIME ZONE '1994-07-20T10:30:00Z', 2, '12345678901', 6500.0, 'Maria Silva');
+INSERT INTO TB_CLIENT (birth_Date, children, cpf, income, name) VALUES (TIMESTAMP WITH TIME ZONE '1994-07-20T10:30:00Z', 2, '12345678901', 6500.0, 'Maria Silva');
+INSERT INTO TB_CLIENT (birth_Date, children, cpf, income, name) VALUES (TIMESTAMP WITH TIME ZONE '1994-07-20T10:30:00Z', 2, '12345678901', 6500.0, 'Maria Silva');
+INSERT INTO TB_CLIENT (birth_Date, children, cpf, income, name) VALUES (TIMESTAMP WITH TIME ZONE '1994-07-20T10:30:00Z', 2, '12345678901', 6500.0, 'Maria Silva');
+INSERT INTO TB_CLIENT (birth_Date, children, cpf, income, name) VALUES (TIMESTAMP WITH TIME ZONE '1994-07-20T10:30:00Z', 2, '12345678901', 6500.0, 'Maria Silva');
+
